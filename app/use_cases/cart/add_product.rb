8b40d06ded9a4574ec128ad1ff480e09372e5490
tondeburin <ettam_lina@protonmail.com>
2025-05-30 00:07:26 -0300

    feat: implement UseCase AddProduct

---
+++ app/use_cases/cart/add_product.rb
@@ -0,0 +1,36 @@
+class Cart::AddProduct
+  attr_reader :cart_class, :product_class
+
+  def initialize(product_class: Product, cart_class: Cart)
+    @product_class = product_class
+    @cart_class = cart_class
+  end
+
+  def call(cart_id, product_id, quantity)
+    cart = find_cart(cart_id)
+    product = find_product(product_id)
+
+    validate_quantity(quantity)
+
+    cart.add_product(product_id, quantity, product)
+    cart.save!
+
+    cart
+  end
+
+  private
+
+  def find_cart(cart_id)
+    cart_class.find_by(id: cart_id) || raise(CartErrors::CartNotFound)
+  end
+
+  def find_product(product_id)
+    product_class.find_by(id: product_id) || raise(CartErrors::ProductNotFound)
+  end
+
+  def validate_quantity(quantity)
+    return if quantity.is_a?(Integer) && quantity.positive?
+
+    raise CartErrors::InvalidQuantity, "Quantidade inválida: #{quantity}"
+  end
+end
