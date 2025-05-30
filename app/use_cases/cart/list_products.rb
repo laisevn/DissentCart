413d1db3cc29abb317bb6d926bbde373476d6d5d
tondeburin <ettam_lina@protonmail.com>
2025-05-30 00:09:06 -0300

    feat: implement UseCase ListProduct

---
+++ app/use_cases/cart/list_products.rb
@@ -0,0 +1,30 @@
+class Cart::ListProducts
+  attr_reader :cart_class
+
+  def initialize(cart_class: Cart)
+    @cart_class = cart_class
+  end
+
+  def call(cart_id)
+    cart = find_cart(cart_id)
+
+    if cart.products.present?
+      CartSerializer.new(cart).as_json
+    else
+      empty_cart_response
+    end
+  end
+
+  private
+
+  def find_cart(cart_id)
+    cart_class.includes(:products).find_by(id: cart_id) || raise(CartErrors::CartNotFound)
+  end
+
+  def empty_cart_response
+    {
+      products: [],
+      total_price: 0.0
+    }
+  end
+end
